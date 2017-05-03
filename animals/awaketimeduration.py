607380b99a08f860e4126b191414b795fcd1ab62
war <thomas@warwaris.at>
2017-05-03 17:48:33 +0200

    documented in doxygen style

--- animals/awaketimeduration.py
+++ animals/awaketimeduration.py
@@ -1,9 +1,19 @@
 class AwakeTimeDuration(object):
+	""" Class to handle the duration an animal is awake
+	"""
+
 	def __init__(self, start, end):
+		""" Constructor
+		@param start hour when the animal awakes
+		@param end hour when the animal falls asleep
+		"""
 		self.start = start
 		self.end = end
 
 	def check_if_in_awake_time_duration(self, time):
+		""" Check if the parameter time is within the awake time of this duration.
+		@param time The time to check
+		"""
 		if self.is_dayactive():
 			status = self._time_in_day_duration(time)
 			return status
@@ -22,6 +32,8 @@ class AwakeTimeDuration(object):
 		return True
 
 	def is_nightactive(self):
+		""" An animal is nightactive if it awakes later on a day than it falls asleep.
+		"""
 		if self.start > self.end:
 			return True
 		else:
